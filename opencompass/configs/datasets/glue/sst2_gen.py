63826ddf54c1dac339f319e887151582e2f1a29c
iamkaia <iamkaia>
2026-05-26 01:52:54 +0800

    0526:增加註解

--- opencompass/configs/datasets/glue/sst2_gen.py
+++ opencompass/configs/datasets/glue/sst2_gen.py
@@ -6,12 +6,14 @@ from opencompass.datasets.sst2_ab import SST2_convert_np
 from opencompass.utils.text_postprocessors import  sst2_postprocess
 
 
+###讀sentence當輸入，label當標準答案，validation 的 datasets split 當評估
 reader_cfg = dict(
     input_columns=['sentence'],
     output_column='label',      # HF SST-2: "0" / "1"
     test_split='validation'
 )
 
+###套PromptTemplate
 infer_cfg = dict(
     prompt_template=dict(
         type=PromptTemplate,
@@ -32,6 +34,7 @@ infer_cfg = dict(
     inferencer=dict(type=GenInferencer), ####不能加max_out_len, model會只輸出奇怪的字
 )
 
+####用Acc evaluator, 前處理用sst2_postprocess回答整理成可比對 label
 eval_cfg = dict(
     evaluator=dict(
         type=AccEvaluator,
@@ -42,6 +45,7 @@ eval_cfg = dict(
     ),
 )
 
+###註冊成一個datasets, abbr是用sst2, type是用SST2_convert_np
 sst2_datasets = [
     dict(
         abbr='sst2',
